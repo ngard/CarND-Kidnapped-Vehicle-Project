512e7672d011b402a2abda02d4537e652cface9e
Tohge, Norihito <tohge@C3PO>
2018-12-04 10:43:22 +0900

    parameter tuning.

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -26,7 +26,7 @@ void ParticleFilter::init(double x, double y, double theta, double std[])
   //   x, y, theta and their uncertainties from GPS) and all weights to 1. 
   // Add random Gaussian noise to each particle.
   // NOTE: Consult particle_filter.h for more information about this method (and others in this file).
-  num_particles = 1000;
+  num_particles = 100;
 
   normal_distribution<double> rand_generator_x(x,std[0]);
   normal_distribution<double> rand_generator_y(y,std[1]);
